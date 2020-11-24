f428f6b1b1b9d8f3bf5f8fe431b143b06df2eaff
royne <royne.chaves@gmail.com>
2020-11-23 22:21:36 -0500

    add firebase config

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAc-2Zxrbm0UD3BgTCkyVCv85aJCzSmBKw",
+  authDomain: "ph-next.firebaseapp.com",
+  databaseURL: "https://ph-next.firebaseio.com",
+  projectId: "ph-next",
+  storageBucket: "ph-next.appspot.com",
+  messagingSenderId: "339780581490",
+  appId: "1:339780581490:web:d9fa7a9ed4e8d4c973aaeb"
+};
+
+export default firebaseConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import firebase, { FirebaseContext } from '../firebase'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FirebaseContext.Provider 
+      value={{
+        firebase
+      }} >
+      <Component {...pageProps} />
+    </FirebaseContext.Provider>
+  )
 }
 
 export default MyApp
