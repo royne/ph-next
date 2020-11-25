5cfb896b213fddfb3aae093885bf86c57b423ad3
royne <royne.chaves@gmail.com>
2020-11-25 14:12:12 -0500

    sow products and format date

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState, useContext} from 'react'
+import styled from '@emotion/styled'
+import DetallesProducto from '../Components/layout/DetallesProducto';
 import Layout from '../Components/layout/Layout';
+import { FirebaseContext } from '../firebase'
+
+const ListadoProductos  = styled.div`
+  background-color: #f3f3f3;
+  .contenedor{
+    max-width: 1200px;
+    width: 95%;
+    padding: 5rem 0;
+    margin: 0 auto;
+    ul{
+      background-color: #FFF;
+    }
+  }
+`
 
 const Home = () => {
+  const [productos, guardarProductos] = useState([])
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarProducto)
+    }
+    obtenerProductos()
+  }, [])
+
+  function manejarProducto(snapshot) {
+    const productos = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+    guardarProductos(productos)
+  }
 
   return (
     <div>
       <Layout>
-        royne
+        <ListadoProductos>
+          <div className="contenedor">
+            <ul>
+              {productos.map(producto => (
+                <DetallesProducto key={producto.id} producto={producto} />
+              ))}
+            </ul>
+          </div>
+        </ListadoProductos>
       </Layout>
     </div>
   )
