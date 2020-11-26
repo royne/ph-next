8357df6f5e98ddf5bc865a7c954936f1742d9ac1
royne <royne.chaves@gmail.com>
2020-11-26 15:28:14 -0500

    add populares

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState, useContext} from 'react'
+import React from 'react'
 import styled from '@emotion/styled'
 import DetallesProducto from '../Components/layout/DetallesProducto';
 import Layout from '../Components/layout/Layout';
-import { FirebaseContext } from '../firebase'
+import useProductos from '../hooks/useProductos'
 
 const ListadoProductos  = styled.div`
   background-color: #f3f3f3;
@@ -18,27 +18,7 @@ const ListadoProductos  = styled.div`
 `
 
 const Home = () => {
-  const [productos, guardarProductos] = useState([])
-
-  const { firebase } = useContext(FirebaseContext);
-
-  useEffect(() => {
-    const obtenerProductos = () => {
-      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarProducto)
-    }
-    obtenerProductos()
-  }, [])
-
-  function manejarProducto(snapshot) {
-    const productos = snapshot.docs.map(doc => {
-      return {
-        id: doc.id,
-        ...doc.data()
-      }
-    })
-    guardarProductos(productos)
-  }
-
+  const productos = useProductos('creado')
   return (
     <div>
       <Layout>
--- pages/populares.js
+++ pages/populares.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import DetallesProducto from '../Components/layout/DetallesProducto';
 import Layout from '../Components/layout/Layout';
+import useProductos from '../hooks/useProductos'
+
+const ListadoProductos = styled.div`
+  background-color: #f3f3f3;
+  .contenedor{
+    max-width: 1200px;
+    width: 95%;
+    padding: 5rem 0;
+    margin: 0 auto;
+    ul{
+      background-color: #FFF;
+    }
+  }
+`
 
 const Populares = () => {
+  const productos = useProductos('votos')
 
   return (
     <div>
       <Layout>
-        <h1>populares</h1>
+        <ListadoProductos>
+          <div className="contenedor">
+            <ul>
+              {productos.map(producto => (
+                <DetallesProducto key={producto.id} producto={producto} />
+              ))}
+            </ul>
+          </div>
+        </ListadoProductos>
       </Layout>
     </div>
   )
