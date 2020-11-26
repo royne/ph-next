eea92d18f1e4dfc532bb5900cd6af7cc3b33c7ba
royne <royne.chaves@gmail.com>
2020-11-26 15:01:10 -0500

    add comments

--- Components/layout/Layout.js
+++ Components/layout/Layout.js
@@ -51,7 +51,6 @@ const Layout = props => {
 
       </Global>
       <Head>
-        <html lang="es" />
         <title>Product Hunt</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -22,6 +22,8 @@ const ContenedorProducto = styled.div`
 const Producto = () => {
   const [producto, guardarProducto] = useState({})
   const [error, guardarError] = useState(false)
+  const [comentario, guardarComentario] = useState({})
+  const [consultaDb, gaurdarConsultaDb] = useState(true)
 
   const router = useRouter()
   const { query: { id }} = router
@@ -29,21 +31,23 @@ const Producto = () => {
   const {firebase, usuario} = useContext(FirebaseContext)
 
   useEffect(() => {
-    if(id) {
+    if (id && consultaDb) {
       const obtenerProducto = async () => {
         const productoQuery = await firebase.db.collection('productos').doc(id)
         const producto = await productoQuery.get()
         if(producto.exists){
           guardarProducto(producto.data())
+          gaurdarConsultaDb(false)
         }else{
           guardarError(true)
+          gaurdarConsultaDb(false)
         }
       }
       obtenerProducto()
     }
-  }, [id, producto])
+  }, [id])
 
-  if(Object.keys(producto).length === 0) return <Layout><p>Cargando.....</p></Layout>
+  if(Object.keys(producto).length === 0 && !error) return <Layout><p>Cargando.....</p></Layout>
 
   const { comentarios, creado, descripcion, empresa, nombre, url, urlImagen, votos, creador, haVotado } = producto;
 
@@ -66,56 +70,113 @@ const Producto = () => {
       ...producto,
       votos: nuevoTotal
     })
+    gaurdarConsultaDb(true)
   }
 
+  // funciones para comentarios
+  const comentarioChange = e => {
+    guardarComentario({
+      ...comentario,
+      [e.target.name] : e.target.value
+    })
+  }
+  
+  const agregarComentario = e => {
+    e.preventDefault()
+    if(!usuario) return router.push('/login')
+    comentario.usuarioId = usuario.uid,
+    comentario.usuarioNombre = usuario.displayName
+    const nuevosComentarios = [...comentarios, comentario]
+    firebase.db.collection('productos').doc(id).update({
+      comentarios: nuevosComentarios
+    })
+    guardarProducto({
+      ...producto,
+      comentarios: nuevosComentarios
+    })  
+  }
+
+  const puedeBorrar = () => {
+    if(!usuario) return false
+    if(creador.id === usuario.uid) return true
+  }
+
+  const eliminarProducto = async () => {
+    if (!usuario) return router.push('/login')
+    if (creador.id === usuario.uid) return router.push('/')
+    try {
+      await firebase.db.collection('productos').doc(id).delete()
+      router.push('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return ( 
     <Layout>
       <>
-        {error && <Error404 />}
-        <div className="contenedor">
-          <h1 css={css`
-            text-align:center;
-            margin-top:3rem;
-          `}>{nombre}</h1>
+        {error ? <Error404 /> : (
+          <div className="contenedor">
+            <h1 css={css`
+              text-align:center;
+              margin-top:3rem;
+            `}>{nombre}</h1>
 
-          <ContenedorProducto>
-            <div>
-              <p>Publicado hace: {formatDistanceToNow(new Date(creado), { locale: es })} </p>
-              <p>Por {creador.nombre} de {empresa}</p>
-              <img src={urlImagen} />
-              <p>{descripcion}</p>
+            <ContenedorProducto>
+              <div>
+                <p>Publicado hace: {formatDistanceToNow(new Date(creado), { locale: es })} </p>
+                <p>Por {creador.nombre} de {empresa}</p>
+                <img src={urlImagen} />
+                <p>{descripcion}</p>
 
-              { usuario && 
-                <>
-                  <h2>Agrega tu comentario</h2>
-                  <form>
-                    <Campo>
-                      <input type="text" name="mensaje" />
-                    </Campo>
-                    <ImputSubmit type="submit" value="Agregar Comentario" />
-                  </form>
-                </>
-              }
+                { usuario && 
+                  <>
+                    <h2>Agrega tu comentario</h2>
+                    <form onSubmit={agregarComentario}>
+                      <Campo>
+                        <input 
+                          type="text" 
+                          name="mensaje"
+                          onChange={comentarioChange} />
+                      </Campo>
+                      <ImputSubmit type="submit" value="Agregar Comentario" />
+                    </form>
+                  </>
+                }
 
-              <h2 css={css`margin:2rem 0;`}>Comentarios</h2>
-              {comentarios.map(elm => (
-                <li>
-                  <p>{elm.nombre}</p>
-                  <p>Escrito por: {elm.usuarioNombre}</p>
-                </li>
-              ))}
-            </div>
-            <aside>
-              <Boton 
-                target="_blank"
-                bgColor="true"
-                href={url}>Visitar Url</Boton>
-              <p css={css`text-align:center;`}>{votos} Votos</p>
-              {usuario && <Boton onClick={botarProducto}>Votar</Boton> }
-            </aside>
-          </ContenedorProducto>
-        </div>
+                <h2 css={css`margin:2rem 0;`}>Comentarios</h2>
+                {comentarios.length === 0 ? "Aún no hay comentarios" : 
+                  (
+                  <ul>
+                    {comentarios.map((elm, i) => (
+                      <li 
+                        key={`${elm.usuarioId}-${i}`}
+                        css={css`
+                          border: 1px solid #e1e1e1;
+                          padding: 2rem;
+                        `}>
+                        <p>{elm.mensaje}</p>
+                        <p>Escrito por: {''}<span>{elm.usuarioNombre}</span></p>
+                      </li>
+                    ))}
+                  </ul> 
+                  )            
+                }
+              </div>
+              <aside>
+                <Boton 
+                  target="_blank"
+                  bgColor="true"
+                  href={url}>Visitar Url</Boton>
+                <p css={css`text-align:center;`}>{votos} Votos</p>
+                {usuario && <Boton onClick={botarProducto}>Votar</Boton> }
+              </aside>
+            </ContenedorProducto>
+            {puedeBorrar() && 
+              <Boton onClick={eliminarProducto}>Eliminar producto</Boton>
+            }
+          </div>
+        )}
       </>
     </Layout>
    );
